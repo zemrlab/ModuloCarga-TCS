bbbed28e097898127806843e99ccffb788f4a481
AndresCcopa <andres.ccopa@unmsm.edu.pe>
2018-06-21 21:37:25 -0500

    modulodecarga en github

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './componentes/App';
 import registerServiceWorker from './registerServiceWorker';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
